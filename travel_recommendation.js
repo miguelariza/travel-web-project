8aba68331e8aa4c2b3f335b940334f0c374a57e5
miguelariza <miguelangel.ariza@gmail.com>
2026-02-26 22:15:49 -0500

    Added card destination inside sidebar and css for cards

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -4,13 +4,13 @@ function togglePanel() {
   
   panel.classList.toggle("open");
   //overlay.classList.toggle("show");
-
-  panel.innerHTML += `<div id="mySidepanel" class="mySidePanel">`;
-  panel.innerHTML += `<a href="javascript:void(0)" class="close" onclick="closeNav()">×</a>`;
-  panel.innerHTML += `</div>`;
 }
 
 function closeNav() {
     const panel = document.getElementById("mySidePanel");
-    panel.classList.toggle("close");
+    panel.classList.remove("open");
+}
+
+function resetSearch() {
+    document.getElementById("search_input").value = "";
 }
